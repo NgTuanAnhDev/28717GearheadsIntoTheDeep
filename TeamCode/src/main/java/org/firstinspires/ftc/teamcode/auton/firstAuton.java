a04eb4aa50256a07d8b061c7f1944592d7b946a6
Nguyễn Tuấn Anh <142076097+NgTuanAnhDev@users.noreply.github.com>
2025-02-12 18:25:12 +0700

    Update firstAuton.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/firstAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auton/firstAuton.java
@@ -4,26 +4,92 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.drive.TankDrive
 
 @Autonomous(name = "firstAuton")
-public class firstAuton extends LinearOpMode
-{
-    public enum FirstAutonState
-    {
-        MOVING_TO_ADJUST_POSITION,
-        ADJUSTING,
+public class firstAuton extends LinearOpMode {
 
+    public enum FirstAutonState {
+        MOVING_TO_SAMPLE,
+        MOVING_TO_POINT,
     }
+    DcMotor verticalSlideMotor;
+    Servo verticalArmServo;
+    Servo verticalClawServo;
+    DcMotor leftMotor;
+    DcMotor rightMotor;
+    ElapsedTime verticalTimer = new ElapsedTime();
+
     @Override
-    public void runOpMode() throws InterruptedException
-    {
-        DcMotor leftMotor = hardwareMap.dcMotor.get("left_motor");
-        DcMotor rightMotor = hardwareMap.dcMotor.get("right_motor");
+    public void runOpMode() throws InterruptedException {
+        leftMotor = hardwareMap.dcMotor.get("leftMotor");
+        rightMotor = hardwareMap.dcMotor.get("rightMotor");
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        verticalSlideMotor = hardwareMap.dcMotor.get("verticalSlide");
 
-        DcMotor verticalSlideMotor = hardwareMap.dcMotor.get("verticalSlide");
+        verticalSlideMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        verticalSlideMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        Servo verticalArmServo = hardwareMap.servo.get("verticalArm");
-        Servo verticalClawServo = hardwareMap.servo.get("verticalClaw");
+        verticalArmServo = hardwareMap.servo.get("verticalArm");
+        verticalClawServo = hardwareMap.servo.get("verticalClaw");
+        waitForStart();
+        FirstAutonState currentState = FirstAutonState.MOVING_TO_POINT;
+        while (opModeIsActive()) {
+            switch (currentState) {
+                case MOVING_TO_SAMPLE:
+                    telemetry.addData("state","Moving to sample");
+                    telemetry.update();
+                    moveForward(1000);
+                    sleep(2000);
+                    takesample();
+                    currentState = FirstAutonState.MOVING_TO_POINT;
+                    break;
+                case MOVING_TO_POINT:
+                    telemetry.addData("State", "Moving to POINT");
+                    telemetry.update();
+                    dropsample();
+                    currentState = FirstAutonState.MOVING_TO_SAMPLE;
+                    break;
+            }
+
+            sleep(500);
+        }
+    }
+
+    private void moveForward(long timeInMillis) {
+        leftMotor.setPower(-0.5);
+        rightMotor.setPower(-0.5);
+        sleep(timeInMillis);
+        stopMotor();
+    }
+
+    private void moveBackward(long timeInMillis) {
+        leftMotor.setPower(0.5);
+        rightMotor.setPower(0.5);
+        sleep(timeInMillis);
+        stopMotor();
+    }
+    private void stopMotor() {
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+    }
+
+    private void takesample() {
+        verticalArmServo.setPosition();
+        verticalClawServo.setPosition();
+    }
+    private  void dropsample(){
+        moveForward(1000);
+        verticalArmServo.setPosition();
+        moveBackward(500);
+        verticalClawServo.setPosition(0.75);
+        moveBackward(1000);
+        verticalArmServo.setPosition();
+        verticalClawServo.setPosition();
 
     }
+
 }
